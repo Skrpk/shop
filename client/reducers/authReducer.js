dc072946cde60dc751a4d3ef9a6b732f309bc483
Vitalii Skrypka <vskry@macmini9710.ddns.softservecom.com>
2017-09-29 19:35:32 +0300

    Add case for successfull sign up

--- client/reducers/authReducer.js
+++ client/reducers/authReducer.js
@@ -1,10 +1,21 @@
-const initialState = {
+import { Map } from 'immutable';
+import isEmpty from 'lodash/isEmpty';
+
+import constants from '../constants/authConstants';
+
+const initialState = Map({
   isAuthenticated: false,
   user: {},
-};
+});
 
 export default(state = initialState, action = {}) => {
   switch (action.type) {
+    ////////////////////////////// probably SIGN_UP_REQUEST is incorrect constant
+    case constants.SIGN_UP_REQUEST: {
+      return state
+        .set('user', action.payload)
+        .set('isAuthenticated', !isEmpty(action.payload));
+    }
     default:
       return state;
   }
